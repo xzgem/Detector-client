446e6f94ea78d50953ea8a3b2d6a2357b8215cf9
ryoua <1794970183@qq.com>
2020-05-12 13:32:46 +0800

    init

---
+++ client/src/main/java/com/ryoua/Application.java
@@ -0,0 +1,15 @@
+package com.ryoua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: RyouA
+ * @Date: 2020/5/12 - 1:18 下午
+ **/
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
